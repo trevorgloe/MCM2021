c87a28072872755c7170f25db9422844600956f9
unknown <trevorgloe@gmail.com>
2021-02-06 09:30:41 -0800

    Finished mol method for growth model

--- growth_bc.m
+++ growth_bc.m
@@ -2,8 +2,8 @@ function [pl,ql,pr,qr] = growth_bc(xl,ul,xr,ur,t)
 %growth_bc boundary condition for growth model
 pl = [0; ul(2)];
 ql = [1; 0];
-pr = [ur(1) - 1; 0];
-qr = [0; 1];
+pr = [ur(1); ur(2)];
+qr = [0; 0];
 
 end
 
